aaf5760d8e819bfdffcb4995b36f3575ccd0e867
Matthew Bolanos <matthewbolanos@gmail.com>
2023-07-15 11:19:36 +0100

    Add link to real solution

--- samples/dotnet/05-Create-ChatGPT-Plugin/MathPlugin/Add.cs
+++ samples/dotnet/05-Create-ChatGPT-Plugin/MathPlugin/Add.cs
@@ -17,7 +17,7 @@ namespace MathPlugin
         }
 
         [OpenApiOperation(operationId: "Add", tags: new[] { "ExecuteFunction" }, Description = "Add two numbers")]
-        [OpenApiParameter(name: "number1", Description = "The first number to add'", Required = true, In = ParameterLocation.Query)]
+        [OpenApiParameter(name: "number1", Description = "The first number to add", Required = true, In = ParameterLocation.Query)]
         [OpenApiParameter(name: "number2", Description = "The second number to add", Required = true, In = ParameterLocation.Query)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "Returns the sum of the two numbers.")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.BadRequest, contentType: "application/json", bodyType: typeof(string), Description = "Returns the error of the input.")]
