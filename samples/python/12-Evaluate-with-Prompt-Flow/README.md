56c3c355a14e0fe3976fcc00bd167f33a3d7e685
Matthew Bolanos <matthewbolanos@gmail.com>
2023-09-15 17:26:06 +0100

    Remove gitignores

--- samples/python/12-Evaluate-with-Prompt-Flow/README.md
+++ samples/python/12-Evaluate-with-Prompt-Flow/README.md
@@ -10,13 +10,13 @@ This console application demonstrates the final solution to the [creating prompt
 
 ## Configuring the sample
 
-The sample can be configured with a `.env` file in the _perform_math_ folder which holds api keys and other secrets and configurations.
+The sample can be configured with a `.env` file which holds api keys and other secrets and configurations.
 
 Make sure you have an
 [Open AI API Key](https://openai.com/api/) or
 [Azure Open AI service key](https://learn.microsoft.com/azure/cognitive-services/openai/quickstart?pivots=rest-api)
 
-Copy the `.env.example` file in the _perform_math_ folder to a new file named `.env`. Then, copy those keys into the `.env` file:
+Copy the `.env.example` file as a new file named `.env`. Then, copy those keys into the `.env` file:
 
 ```
 GLOBAL__LLM_SERVICE="OpenAI"
@@ -26,12 +26,6 @@ AZURE_OPEN_AI__CHAT_COMPLETION_DEPLOYMENT_NAME="gpt-35-turbo"
 AZURE_OPEN_AI__TEXT_COMPLETION_DEPLOYMENT_NAME="text-davinci-003"
 AZURE_OPEN_AI__ENDPOINT=""
 AZURE_OPEN_AI__API_KEY=""
-
-OPEN_AI__MODEL_TYPE="chat-completion"
-OPEN_AI__CHAT_COMPLETION_MODEL_ID="gpt-3.5-turbo"
-OPEN_AI__TEXT_COMPLETION_MODEL_ID="text-davinci-003"
-OPEN_AI__API_KEY=""
-OPEN_AI__ORG_ID=""
 ```
 
 ## Running the sample
--- samples/python/12-Evaluate-with-Prompt-Flow/perform_math/math_planner.py
+++ samples/python/12-Evaluate-with-Prompt-Flow/perform_math/math_planner.py
@@ -4,25 +4,52 @@ from promptflow import tool
 import semantic_kernel as sk
 from semantic_kernel.planning.sequential_planner import SequentialPlanner
 from plugins.MathPlugin.Math import Math as Math
+from promptflow.connections import (
+    AzureOpenAIConnection,
+)
 
-import config.add_completion_service
+from semantic_kernel.connectors.ai.open_ai import (
+    AzureChatCompletion,
+    AzureTextCompletion,
+)
 
 
 @tool
-def my_python_tool(input1: str) -> str:
+def my_python_tool(
+    input: str,
+    deployment_type: str,
+    deployment_name: str,
+    AzureOpenAIConnection: AzureOpenAIConnection,
+) -> str:
     # Initialize the kernel
-    kernel = sk.Kernel()
-    # Add a text or chat completion service using either:
-    # kernel.add_text_completion_service()
-    # kernel.add_chat_service()
-    kernel.add_completion_service()
+    kernel = sk.Kernel(log=sk.NullLogger())
+
+    # Add the chat service
+    if deployment_type == "chat-completion":
+        kernel.add_chat_service(
+            "chat_completion",
+            AzureChatCompletion(
+                deployment_name,
+                AzureOpenAIConnection.api_base,
+                AzureOpenAIConnection.api_key,
+            ),
+        )
+    elif deployment_type == "text-completion":
+        kernel.add_text_completion_service(
+            "text_completion",
+            AzureTextCompletion(
+                deployment_name,
+                AzureOpenAIConnection.api_base,
+                AzureOpenAIConnection.api_key,
+            ),
+        )
 
     planner = SequentialPlanner(kernel=kernel)
 
     # Import the native functions
     math_plugin = kernel.import_skill(Math(), "MathPlugin")
 
-    ask = "Use the available math functions to solve this word problem: " + input1
+    ask = "Use the available math functions to solve this word problem: " + input
 
     plan = asyncio.run(planner.create_plan_async(ask))
 
